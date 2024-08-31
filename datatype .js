54d4d79789c3aa9e8f5e9408c6b3ad6300dfdcc0
samrinakhter <145998957+samrinakhter@users.noreply.github.com>
2024-08-30 22:58:00 -0700

    Add files via upload

---
+++ datatype .js
@@ -0,0 +1,36 @@
+// ***********datatype***********
+//number
+let age=20;
+let height=5;
+console.log(age,height);
+//string
+let fullname ="samreen akhtar";
+let department ="BSCS";
+console.log(fullname,department);
+//Boolean
+let pass ="true";
+console.log(pass);
+//undefined
+let x;
+console.log(x);
+//null
+let y =null;
+console.log(y);
+//************conversion************
+let num ="samreem akhtar";
+console.log(typeof (num))
+let valueInNumber = "number(num)";
+console.log(valueInNumber);
+//***********operator ************
+let value = 46;
+value++;
+console.log(value)
+//*************combination of two string**************
+let str1 ="samreen";
+let str2 ="akhtar";
+let str3 =str1 +str2;
+console.log(str3)
+//**************combination of str and num************
+console.log ("samreen" + 1 +2);
+console.log(1 +2 + "samreen");
+console.log(1 +2 * 5);
