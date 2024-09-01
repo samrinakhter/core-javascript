e39a99915a5fea6fcacbbf1ceb44d86c07abca71
samrinakhter <145998957+samrinakhter@users.noreply.github.com>
2024-09-01 00:49:46 -0700

    Add files via upload

---
+++ comparsion .js
@@ -0,0 +1,27 @@
+//*****************comparsion operator**********************
+let a =3;
+let b =4;
+console.log(a<b);
+console.log(a>b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a==b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a!==b);
+//****************************comparison with null*****************************
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+//**************************comparison with undefined*************************** 
+console.log(undefined>0);
+console.log(undefined>=0);
+//************************comparison with string********************************* 
+console.log("s">"A");
+console.log('4'>'A');
+console.log('a'>'A');
+
+
+
+
+
